2c952097fb2447ea484b591e6d8cd165163b9742
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2024-01-17 11:20:04 +0100

    test division definitivo

--- test/DivisioTest.java
+++ test/DivisioTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DivisioTest {
 
     @org.junit.jupiter.api.Test
-    void  divisio(){
+    void  divisioCorrecta(){
         int num1 = 10;
         int num2 = 2;
         int res = 5;
@@ -15,5 +15,18 @@ class DivisioTest {
         int num2 = 5;
         int res = 5;
         assertNotEquals(res,Calculadora.divisio(num1,num2));
+    }
+    @org.junit.jupiter.api.Test
+    void  divisioAmbReultatObligatori() {
+        int num1 = 10;
+        int num2 = 5;
+        assertNotNull(Calculadora.divisio(num1, num2));
+    }
+    @org.junit.jupiter.api.Test
+    void  divisioNegativaCorrecta(){
+        int num1 = -10;
+        int num2 = -2;
+        int res = 5;
+        assertEquals(res,Calculadora.divisio(num1,num2));
     }
 }
