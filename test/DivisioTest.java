c7847219ce234ee94c922ca730bad3ef0a3cda93
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2024-01-17 10:56:25 +0100

    test bien

--- test/DivisioTest.java
+++ test/DivisioTest.java
@@ -9,4 +9,11 @@ class DivisioTest {
         int res = 5;
         assertEquals(res,Calculadora.divisio(num1,num2));
     }
+    @org.junit.jupiter.api.Test
+    void  divisioIncorrecte(){
+        int num1 = 10;
+        int num2 = 5;
+        int res = 5;
+        assertNotEquals(res,Calculadora.divisio(num1,num2));
+    }
 }
