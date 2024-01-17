3fee11ea298e2f97c63b79458c6236924986d0bd
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2024-01-17 11:23:08 +0100

    test division definitivo

--- test/DivisioTest.java
+++ test/DivisioTest.java
@@ -13,7 +13,7 @@ class DivisioTest {
     void  divisioIncorrecte(){
         int num1 = 10;
         int num2 = 5;
-        int res = 5;
+        int res = 7;
         assertNotEquals(res,Calculadora.divisio(num1,num2));
     }
     @org.junit.jupiter.api.Test
